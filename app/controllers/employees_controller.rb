6e69b7a75746a4f20cf2be0f9942267f90f454f4
ben-scully <ben@moregolf.co.nz>
2016-05-23 13:17:31 +1200

    working index,create,edit,show, update for Employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,6 @@
 class EmployeesController < ApplicationController
   def index
-    @articles = Article.all
+    @employees = Employee.all
   end
 
   def show
@@ -8,17 +8,31 @@ class EmployeesController < ApplicationController
   end
 
   def new
+    @employee = Employee.new
   end
 
-  def create
-    render plain: params[:employee].inspect
+  def edit
+    @employee = Employee.find(params[:id])
   end
 
   def create
     @employee = Employee.new(employee_params)
 
-    @employee.save
-    redirect_to @employee
+    if @employee.save
+      redirect_to @employee
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+
+    if @employee.update(employee_params)
+      redirect_to @employee
+    else
+      render 'edit'
+    end
   end
 
   private
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ActiveRecord::Base
+  validates :name, presence: true,
+                   length: { minimum: 3 }
+  validates :email, presence: true,
+                    length: { minimum: 3 },
+                    uniqueness: true
 end
